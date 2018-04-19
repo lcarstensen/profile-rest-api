1a2d0dec9039c2dfab565636703f8eaa0f65388a
Lans Carstensen <lans@carstensen.cx>
2018-04-18 22:13:02 -0700

    created first api

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Profiles REST API
 
 REST API providing basic functionality for managing user profiles.
+
+admintest@carstensen.cx / adminadmin
+test@carstensen.cx / test
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'User HTTP methods as functions',
+            'It is simple',
+            'Gives you the most control',
+            'Is mapped manually'
+        ]
+
+        return Response({'message': 'Hello World!', 'an_apiview': an_apiview})
